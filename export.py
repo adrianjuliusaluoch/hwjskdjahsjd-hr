f08761b4574c31ac25a29d0c2f7ac19069234a1e
Adrian Julius Aluoch <adrianjuliusaluoch@gmail.com>
2025-09-11 18:14:35 +0300

    Create export.py

---
+++ export.py
@@ -0,0 +1,24 @@
+# Import Packages
+from google.cloud import bigquery
+import pandas as pd
+
+# Initialize Client
+client = bigquery.Client(project='crypto-stocks-01')
+
+# Define SQL Query to Retrieve Crypto Data
+query = """
+    SELECT * 
+    FROM `crypto-stocks-01.storage.top_cryptocurrency`
+    ORDER BY timestamp DESC
+"""
+crypto = client.query(query).to_dataframe()
+crypto.to_csv("storage/cryptocurrency.csv", index=False)
+
+# Define SQL Query to Retrieve Stocks Data
+query = """
+    SELECT * 
+    FROM `crypto-stocks-01.storage.top_stocks`
+    ORDER BY timestamp DESC
+"""
+stocks = client.query(query).to_dataframe()
+stocks.to_csv("storage/stocks.csv", index=False)
