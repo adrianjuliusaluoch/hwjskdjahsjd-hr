a66f060f24b2dafd082fa0aa00525125aa087479
Adrian Julius Aluoch <adrianjuliusaluoch@gmail.com>
2026-01-26 16:57:54 +0300

    Update export.py

--- export.py
+++ export.py
@@ -1,25 +1,39 @@
 # Import Packages
 from google.cloud import bigquery
 import pandas as pd
+import datetime
+import os  
 
 # Initialize Client
-client = bigquery.Client(project='crypto-stocks-01')
+client = bigquery.Client()
 
-# Define SQL Query to Retrieve Crypto Data
-query = """
-    SELECT * 
-    FROM `crypto-stocks-01.storage.top_cryptocurrency`
-    ORDER BY timestamp DESC
-"""
-crypto = client.query(query).to_dataframe()
+now = datetime.datetime.now()  # Use datetime.datetime
+year = now.year
+month = now.strftime("%b").lower()  # jan, feb, mar
 
-# Define SQL Query to Retrieve Stocks Data
-query = """
-    SELECT * 
-    FROM `crypto-stocks-01.storage.top_stocks`
-    ORDER BY timestamp DESC
-"""
-stocks = client.query(query).to_dataframe()
+table_suffix = f"{year}_{month}"
+
+# Define Table ID
+table_id = f"data-storage-485106.investing.crypto_{table_suffix}"
+
+# Define SQL Query to Retrieve All Records from BigQuery
+sql = (f"""
+        SELECT *
+        FROM `{table_id}`
+       """)
+
+crypto = client.query(sql).to_dataframe()
+
+# Define Table ID
+table_id = f"data-storage-485106.investing.stocks_{table_suffix}"
+
+# Define SQL Query to Retrieve All Records from BigQuery
+sql = (f"""
+        SELECT *
+        FROM `{table_id}`
+       """)
+
+stocks = client.query(sql).to_dataframe()
 
 crypto.to_csv("storage/cryptocurrency.csv", index=False, encoding='utf-8')
 stocks.to_csv("storage/stocks.csv", index=False, encoding='utf-8')
